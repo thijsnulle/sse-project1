a0127e736cc67dc1f5b1a8f1539c6bd868f9ab3f
Thijs Nulle <thijsnulle@Thijss-MacBook-Pro.local>
2024-02-19 13:24:19 +0100

    Add initial implementation of React Latest app

---
+++ populate-data.py
@@ -0,0 +1,54 @@
+import json, os, random, string
+
+FOLDERS = ['react-latest']
+NUM_DATA_POINTS = 10000
+HOBBIES = [
+    'swimming',
+    'running',
+    'cycling',
+    'hiking',
+    'reading',
+    'writing',
+    'cooking',
+    'baking',
+    'gardening',
+    'painting',
+    'drawing',
+    'knitting',
+    'sewing',
+    'woodworking',
+    'metalworking',
+    'pottery',
+    'sculpting',
+    'photography',
+    'videography',
+    'music',
+    'singing',
+    'dancing',
+    'acting',
+    'yoga',
+    'pilates',
+    'boxing',
+    'martial arts',
+    'meditation',
+]
+
+def populate_data(data, folder: str):
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    data_file = os.path.join(current_dir, folder, 'src', 'data.json')
+
+    with open(data_file, 'w') as f:
+        json.dump(data, f)
+
+if __name__ == '__main__':
+    data = [
+        {
+            'id': random.randint(1, 100),
+            'name': ''.join(random.choice(string.ascii_uppercase) for _ in range(48)),
+            'age': random.randint(1, 100),
+            'hobbies': random.sample(HOBBIES, random.randint(len(HOBBIES) // 2, len(HOBBIES)))
+        } for _ in range(NUM_DATA_POINTS)
+    ]
+
+    for folder in FOLDERS:
+        populate_data(data, folder)
--- react-latest/src/App.js
+++ react-latest/src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import _data from './data.json';
+
 import './App.css';
 
-function App() {
+const App = () => {
+  const [data, setData] = useState(_data);
+
+  const headers = data.length > 0 ? Object.keys(data[0]) : [];
+
+  const reset = () => setData(_data);
+  const filter = (fn) => setData(data.filter(fn));
+  const reverse = () => setData(data.slice().reverse());
+  const random = () => setData(data.sort(() => Math.random() - 0.5));
+  const map = () => setData(data.map((row) => ({
+    ...row,
+    age: row.age + 1,
+    name: row.name.toLowerCase()
+  })));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React App</h1>
+
+      <div className="buttons">
+        <button onClick={reset} className="reset">
+          Reset
+        </button>
+
+        <button onClick={() => filter((row) => row.id < 90)} className="filter-id">
+          Filter ID
+        </button>
+
+        <button onClick={() => filter((row) => row.name.toLowerCase().includes('k'))} className="filter-name">
+          Filter Name
+        </button>
+
+        <button onClick={() => filter((row) => row.age > 20)} className="filter-age">
+          Filter Age
+        </button>
+
+        <button onClick={() => filter((row) => row.hobbies.includes('swimming'))} className="filter-hobbies">
+          Filter Hobbies
+        </button>
+
+        <button onClick={reverse} className="reverse">
+          Reverse
+        </button>
+
+        <button onClick={random} className="random">
+          Random
+        </button>
+
+        <button onClick={map} className="map">
+          Map
+        </button>
+      </div>
+
+      <h4>Table ({data.length} rows)</h4>
+
+      <table>
+        <thead>
+          {headers.map((header, index) => (
+            <th key={index}>{header}</th>
+          ))}
+        </thead>
+
+        <tbody>
+          {data.map((row, index) => (
+            <tr key={index}>
+              {headers.map((header, index) => (
+                <td key={index}>{row[header]}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
