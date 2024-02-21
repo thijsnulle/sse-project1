725e156e8a61c23d5ad638808b3adbc98eb16b32
HarmenKroon <H.M.Kroon@student.tudelft.nl>
2024-02-21 10:02:12 +0100

    Typo fix

--- EnergiBridge.py
+++ EnergiBridge.py
@@ -13,10 +13,11 @@ class EnergiBridge:
         program_path = os.path.join(os.path.dirname(__file__), "..", "energibridge", "energibridge")
         if sys.platform == "win32":
             program_path += ".exe"
-        # print(["'"+program_path+'"', "-i", str(self.settings.interval), "--max-execution", str(task.workload.max_execution), "-o", '"'+f"{task.output_path}.csv"+'"', "--command-output", '"'+f"{task.output_path}.log"+'"'])
-        return ["'"+program_path+'"', "-i", str(self.settings.interval), "--max-execution", str(task.workload.max_execution), "-o", '"'+f"{task.output_path}.csv"+'"', "--command-output", '"'+f"{task.output_path}.log"+'"']
+        # print(program_path)
+        # print(['"'+program_path+'"', "-i", str(self.settings.interval), "--max-execution", str(task.workload.max_execution), "-o", '"'+f"{task.output_path}.csv"+'"'])
+        return ['"'+program_path+'"', "-i", str(self.settings.interval), "--max-execution", str(task.workload.max_execution), "-o", '"'+f"{task.output_path}.csv"+'"']
 
     def run(self, task: runner.Task):
         # BUG/TODO:  Add sleep/timeout after --. Just running `--` will not run the energibridge. Because we open the website via the webdriver it does not make sense to open here again.
-        # ERROR: `De syntaxis van de bestandsnaam, mapnaam of volumenaam is onjuist.`
+        # ERROR: `Input redirection is not supported, exiting the process immediately.` -> using timeout in subprocess
         task.Current_Energibridge_Process = subprocess.Popen(" ".join(self.cmd(task) + ['--summary timeout /t 20']), shell=True) # , task.workload.command
